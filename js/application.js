bf81ac9564b60e9ad84f2373e420f2895f44490f
Colin Ardern <cardern@gmail.com>
2015-03-17 16:38:00 -0500

    Improve array formatting

--- js/application.js
+++ js/application.js
@@ -14,116 +14,168 @@ $(document).ready(function(){
 		return Math.floor(Math.random()*rList.length);
 	};
 
-	rList.push("Vietnamese Noodles");
-	rList.push("North Shore Bistro");
-	rList.push("Ferrante's");
-	rList.push("California Pizza Kitchen");
-	rList.push("Potbelly Sandwiches");
-	rList.push("Noodles");
-	rList.push("Bruegger's Bagels");
-	rList.push("Cousin's Subs");
-	rList.push("Subway");
-	rList.push("Panera Bread");
-	rList.push("Culver's");
-	rList.push("Brew City Barbecue");
-	rList.push("Fiddleheads");
-	rList.push("Chancery");
-	rList.push("Melthouse Bistro");
-	rList.push("Bar Louie");
-	rList.push("Zaffiro's");
-	rList.push("Devon Seafood Grill");
-	rList.push("Milwaukee Ale House");
-	rList.push("Fork (At Outpost)");
-	rList.push("Jimmy John's");
-	rList.push("Centennial");
-	rList.push("Jose's Blue Sombrero");
-	rList.push("Leonardo's Pizza");
-	rList.push("Highland House");
-	rList.push("The Roman Candle");
-	rList.push("Cheesecake Factory");
-	rList.push("Toppers Pizza");
-	rList.push("Cafe 1505");
-	rList.push("Tomaso's Pizzeria");
-	rList.push("Fresh Market (Deli Menu)");
-	rList.push("Fresh (at Pavillion)");
-	rList.push("Blaze Pizza");
-	rList.push("Sprecher's Pub");
-	rList.push("Big Daddy's BBQ");
-	rList.push("Quizno's");
-	rList.push("Out & Out Custard and Sandwiches");
-	rList.push("Water Street Brewery (Grafton)");
-	rList.push("Boston Market");
-	rList.push("Ian's Pizza");
-	rList.push("Silver Spring House");
-	rList.push("Applebee's");
-	rList.push("Cosi");
-	rList.push("Einstein Bros Bagels");
-	rList.push("The Charcoal Grill");
-	rList.push("El Paisa");
-	rList.push("Taco Bell");
-	rList.push("Taylor and Dunn's");
-	rList.push("Benji's Deli");
-	rList.push("The Anchorage");
-	rList.push("Remington's");
-	rList.push("Range Line Inn");
-	rList.push("Five Guys");
-	rList.push("Poco Loco");
-
+	rList.push("Vietnamese Noodles"); 
 	rLink.push("http://vietnamesenoodles.us/menu/");
-	rLink.push("http://www.nsbbarandgrill.com/menu-entrees.html");
-	rLink.push("http://www.ferrantes.com/menu.html");
-	rLink.push("http://www.cpk.com/menu/");
-	rLink.push("http://menus.potbelly.com/PB_FaxMenu_041.pdf");
-	rLink.push("http://www.noodles.com/food/");
-	rLink.push("https://www.brueggers.com/menu/lunch-sandwiches");
-	rLink.push("http://www.cousinssubs.com/menu/sub-sandwiches/sub-sandwiches");
-	rLink.push("http://www.subway.com/menu/MenuCategoryItems.aspx?CC=USA&LC=ENG&MenuTypeId=1&MenuId=35");
-	rLink.push("https://www.panerabread.com/content/dam/panerabread/documents/nutrition/Menu-Retail.PDF");
-	rLink.push("http://www.culvers.com/menu-and-nutrition/");
-	rLink.push("http://www.allmenus.com/wi/milwaukee/9604-brew-city-bbq-downtown/menu/");
-	rLink.push("https://www.fiddleheadscoffee.com/fiddleheads_bakery_store.aspx");
-	rLink.push("http://www.thechancery.com/wp-content/uploads/Chancery-Core-Menu-White-Bi-Fold-23-Final.pdf");
-	rLink.push("http://www.melthousebistro.com/cmsAdmin/uploads/MHB_Take_Out_Menu_Revised_April_2014_web.pdf");
-	rLink.push("http://www.barlouieamerica.com/menu/eat/plates.aspx#menu");
-	rLink.push("http://zaffirospizzeria.com/downloads/ZaffirosMenuWeb_052214.pdf");
-	rLink.push("http://www.devonseafood.com/menus/dinner/164.pdf");
-	rLink.push("http://ale-house.com/wp-content/uploads/2013/09/DinnerWithBeer.pdf");
-	rLink.push("http://www.outpost.coop/userimages/J4115d%20FORK%20to%20go%20menu%20Mequon%2010_14(1).pdf");
-	rLink.push("https://www.jimmyjohns.com/menu/#/");
-	rLink.push("http://www.centennialbg.com/menu/");
-	rLink.push("http://www.josesbluesombrero.com/menu/");
-	rLink.push("http://www.leonardospizzamequon.com/menu.htm");
-	rLink.push("http://0101.nccdn.net/1_5/34c/2d9/19b/Dinner-menu-PRINT--3-.pdf");
-	rLink.push("http://theromancandle.com/wp-content/uploads/2012/01/TRC_MSN_Fall2014Menu_November2014.pdf");
-	rLink.push("http://www.thecheesecakefactory.com/menu/");
-	rLink.push("http://www.toppers.com/menu");
-	rLink.push("http://www.cafe1505.com/menu/dinner");
-	rLink.push("http://www.tomasoscedarburg.com/menu.cfm?id=11");
-	rLink.push("");
-	rLink.push("");
-	rLink.push("http://www.blazepizza.com/menu/signature-pizzas");
-	rLink.push("http://www.sprecherspub.com/welcome/#dinner-menu");
-	rLink.push("http://bigdaddysbbq-stpaul.com/");
-	rLink.push("http://www.quiznos.com/Menu/Food-menu.aspx");
-	rLink.push("http://www.outandoutcustard.com/files/Menu.pdf");
-	rLink.push("https://www.waterstreetbrewery.com/images/menus/downtownfullmenu.pdf");
-	rLink.push("http://www.bostonmarket.com/ourFood?page=foodSub&id=1&name=Individual+Meals");
-	rLink.push("http://ianspizza.com/menus/");
-	rLink.push("http://www.thesilverspringhouse.com/dinnermenu.php");
-	rLink.push("http://www.applebees.com/menu/entrees");
-	rLink.push("https://getcosi.com/menu/entrees.html");
-	rLink.push("http://www.einsteinbros.com/menu/6/anytime-creations");
-	rLink.push("http://www.charcoalgrill.com/menu-burgers.html");
-	rLink.push("http://www.lospaisa.com/menu");
-	rLink.push("http://www.tacobell.com/food/menu/fourth-meal");
-	rLink.push("http://tayloranddunns.com/menu/");
-	rLink.push("http://www.benjisdeliandrestaurant.com/uploads/2/2/5/1/22511402/benjis_091114.pdf");
-	rLink.push("http://www.foodspot.com/Search/external.aspx?fs=3&url=http://www.foodspot.com/Clients/WI/Milwaukee/TheAnchorage/Anchorage%20Breakfast221.pdf");
-	rLink.push("http://remingtonsriverinn.com/menu.html");
-	rLink.push("http://www.therangelineinn.com/Media/Range_Line_Menu.pdf");
-	rLink.push("http://www.fiveguys.com/menu.aspx");
-	rLink.push("http://www.pocolococantina.com/media/PocoLoco_menu.pdf");
 
+	rList.push("North Shore Bistro"); 
+	rLink.push("http://www.nsbbarandgrill.com/menu-entrees.html");
+
+	rList.push("Ferrante's"); 
+	rLink.push("http://www.ferrantes.com/menu.html");
+
+	rList.push("California Pizza Kitchen"); 
+	rLink.push("http://www.cpk.com/menu/");
+
+	rList.push("Potbelly Sandwiches"); 
+	rLink.push("http://menus.potbelly.com/PB_FaxMenu_041.pdf");
+
+	rList.push("Noodles"); 
+	rLink.push("http://www.noodles.com/food/");
+
+	rList.push("Bruegger's Bagels"); 
+	rLink.push("https://www.brueggers.com/menu/lunch-sandwiches");
+
+	rList.push("Cousin's Subs"); 
+	rLink.push("http://www.cousinssubs.com/menu/sub-sandwiches/sub-sandwiches");
+
+	rList.push("Subway"); 
+	rLink.push("http://www.subway.com/menu/MenuCategoryItems.aspx?CC=USA&LC=ENG&MenuTypeId=1&MenuId=35");
+
+	rList.push("Panera Bread"); 
+	rLink.push("https://www.panerabread.com/content/dam/panerabread/documents/nutrition/Menu-Retail.PDF");
+
+	rList.push("Culver's"); 
+	rLink.push("http://www.culvers.com/menu-and-nutrition/");
+
+	rList.push("Brew City Barbecue"); 
+	rLink.push("http://www.allmenus.com/wi/milwaukee/9604-brew-city-bbq-downtown/menu/");
+
+	rList.push("Fiddleheads"); 
+	rLink.push("https://www.fiddleheadscoffee.com/fiddleheads_bakery_store.aspx");
+
+	rList.push("Chancery"); 
+	rLink.push("http://www.thechancery.com/wp-content/uploads/Chancery-Core-Menu-White-Bi-Fold-23-Final.pdf");
+
+	rList.push("Melthouse Bistro"); 
+	rLink.push("http://www.melthousebistro.com/cmsAdmin/uploads/MHB_Take_Out_Menu_Revised_April_2014_web.pdf");
+
+	rList.push("Bar Louie"); 
+	rLink.push("http://www.barlouieamerica.com/menu/eat/plates.aspx#menu");
+
+	rList.push("Zaffiro's"); 
+	rLink.push("http://zaffirospizzeria.com/downloads/ZaffirosMenuWeb_052214.pdf");
+
+	rList.push("Devon Seafood Grill"); 
+	rLink.push("http://www.devonseafood.com/menus/dinner/164.pdf");
+
+	rList.push("Milwaukee Ale House"); 
+	rLink.push("http://ale-house.com/wp-content/uploads/2013/09/DinnerWithBeer.pdf");
+
+	rList.push("Fork (At Outpost)"); 
+	rLink.push("http://www.outpost.coop/userimages/J4115d%20FORK%20to%20go%20menu%20Mequon%2010_14(1).pdf");
+
+	rList.push("Jimmy John's"); 
+	rLink.push("https://www.jimmyjohns.com/menu/#/");
+
+	rList.push("Centennial"); 
+	rLink.push("http://www.centennialbg.com/menu/");
+
+	rList.push("Jose's Blue Sombrero"); 
+	rLink.push("http://www.josesbluesombrero.com/menu/");
+
+	rList.push("Leonardo's Pizza"); 
+	rLink.push("http://www.leonardospizzamequon.com/menu.htm");
+
+	rList.push("Highland House"); 
+	rLink.push("http://0101.nccdn.net/1_5/34c/2d9/19b/Dinner-menu-PRINT--3-.pdf");
+
+	rList.push("The Roman Candle"); 
+	rLink.push("http://theromancandle.com/wp-content/uploads/2012/01/TRC_MSN_Fall2014Menu_November2014.pdf");
+
+	rList.push("Cheesecake Factory"); 
+	rLink.push("http://www.thecheesecakefactory.com/menu/");
+
+	rList.push("Toppers Pizza"); 
+	rLink.push("http://www.toppers.com/menu");
+
+	rList.push("Cafe 1505"); 
+	rLink.push("http://www.cafe1505.com/menu/dinner");
+
+	rList.push("Tomaso's Pizzeria"); 
+	rLink.push("http://www.tomasoscedarburg.com/menu.cfm?id=11");
+
+	rList.push("Fresh Market (Deli Menu)"); 
+	rLink.push("");
+
+	rList.push("Fresh (at Pavillion)"); 
+	rLink.push("");
+
+	rList.push("Blaze Pizza"); 
+	rLink.push("http://www.blazepizza.com/menu/signature-pizzas");
+
+	rList.push("Sprecher's Pub"); 
+	rLink.push("http://www.sprecherspub.com/welcome/#dinner-menu");
+
+	rList.push("Big Daddy's BBQ"); 
+	rLink.push("http://bigdaddysbbq-stpaul.com/");
+
+	rList.push("Quizno's"); 
+	rLink.push("http://www.quiznos.com/Menu/Food-menu.aspx");
+
+	rList.push("Out & Out Custard and Sandwiches"); 
+	rLink.push("http://www.outandoutcustard.com/files/Menu.pdf");
+
+	rList.push("Water Street Brewery (Grafton)"); 
+	rLink.push("https://www.waterstreetbrewery.com/images/menus/downtownfullmenu.pdf");
+
+	rList.push("Boston Market"); 
+	rLink.push("http://www.bostonmarket.com/ourFood?page=foodSub&id=1&name=Individual+Meals");
+
+	rList.push("Ian's Pizza"); 
+	rLink.push("http://ianspizza.com/menus/");
+
+	rList.push("Silver Spring House"); 
+	rLink.push("http://www.thesilverspringhouse.com/dinnermenu.php");
+
+	rList.push("Applebee's"); 
+	rLink.push("http://www.applebees.com/menu/entrees");
+
+	rList.push("Cosi"); 
+	rLink.push("https://getcosi.com/menu/entrees.html");
+
+	rList.push("Einstein Bros Bagels"); 
+	rLink.push("http://www.einsteinbros.com/menu/6/anytime-creations");
+
+	rList.push("The Charcoal Grill"); 
+	rLink.push("http://www.charcoalgrill.com/menu-burgers.html");
+
+	rList.push("El Paisa"); 
+	rLink.push("http://www.lospaisa.com/menu");
+
+	rList.push("Taco Bell"); 
+	rLink.push("http://www.tacobell.com/food/menu/fourth-meal");
+
+	rList.push("Taylor and Dunn's"); 
+	rLink.push("http://tayloranddunns.com/menu/");
+
+	rList.push("Benji's Deli"); 
+	rLink.push("http://www.benjisdeliandrestaurant.com/uploads/2/2/5/1/22511402/benjis_091114.pdf");
+
+	rList.push("The Anchorage"); 
+	rLink.push("http://www.foodspot.com/Search/external.aspx?fs=3&url=http://www.foodspot.com/Clients/WI/Milwaukee/TheAnchorage/Anchorage%20Breakfast221.pdf");
+
+	rList.push("Remington's"); 
+	rLink.push("http://remingtonsriverinn.com/menu.html");
+
+	rList.push("Range Line Inn"); 
+	rLink.push("http://www.therangelineinn.com/Media/Range_Line_Menu.pdf");
+
+	rList.push("Five Guys"); 
+	rLink.push("http://www.fiveguys.com/menu.aspx");
+
+	rList.push("Poco Loco"); 
+	rLink.push("http://www.pocolococantina.com/media/PocoLoco_menu.pdf");
+	
 
 	$(".versus").hide();
 	$(".pickEat").on("click",function(){
